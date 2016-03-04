758392f7613fc631e85484df85f4ec3e9c5f4701
Percipience23 <percipience23@gmail.com>
2016-03-03 18:25:17 -0600

    Deleted a lot of useless whitespace
    Deleted a lot of useless whitespace

--- addon/gmangui/lua/entities/gman/init.lua
+++ addon/gmangui/lua/entities/gman/init.lua
@@ -1,38 +1,27 @@
-
-AddCSLuaFile( "cl_init.lua" ) -- This means the client will download these files
-AddCSLuaFile( "shared.lua" )
+AddCSLuaFile("cl_init.lua") -- This means the client will download these files
+AddCSLuaFile("shared.lua")
 
 include('shared.lua') -- At this point the contents of shared.lua are ran on the server only.
 
-
-function ENT:Initialize( ) --This function is run when the entity is created so it's a good place to setup our entity.
-	
-	self:SetModel( HelperNPC.NPCModel ) -- Sets the model of the NPC.
-	self:SetHullType( HULL_HUMAN ) -- Sets the hull type, used for movement calculations amongst other things.
-	self:SetHullSizeNormal( )
-	self:SetNPCState( NPC_STATE_SCRIPT )
-	self:SetSolid(  SOLID_BBOX ) -- This entity uses a solid bounding box for collisions.
-	self:CapabilitiesAdd( CAP_ANIMATEDFACE) -- Adds what the NPC is allowed to do ( It cannot move in this case ).
-	self:SetUseType( SIMPLE_USE ) -- Makes the ENT.Use hook only get called once at every use.
+function ENT:Initialize() --This function is run when the entity is created so it's a good place to setup our entity.
+	self:SetModel(HelperNPC.NPCModel) -- Sets the model of the NPC.
+	self:SetHullType(HULL_HUMAN) -- Sets the hull type, used for movement calculations amongst other things.
+	self:SetHullSizeNormal()
+	self:SetNPCState(NPC_STATE_SCRIPT)
+	self:SetSolid(SOLID_BBOX) -- This entity uses a solid bounding box for collisions.
+	self:CapabilitiesAdd(CAP_ANIMATEDFACE) -- Adds what the NPC is allowed to do ( It cannot move in this case ).
+	self:SetUseType(SIMPLE_USE) -- Makes the ENT.Use hook only get called once at every use.
 	self:DropToFloor()
-	
-	self:SetMaxYawSpeed( 90 ) --Sets the angle by which an NPC can rotate at once.
-	
+	self:SetMaxYawSpeed(90) --Sets the angle by which an NPC can rotate at once.
 end
 
 function ENT:OnTakeDamage()
 	return false -- This NPC won't take damage from anything.
-end 
-
-function ENT:AcceptInput( Name, Activator, Caller )	
-
-	if Name == "Use" and Caller:IsPlayer() then
-		
-		umsg.Start("OpenHowto", Caller) -- Prepare the usermessage to that same player to open the menu on his side.
-		umsg.End() -- We don't need any content in the usermessage so we're sending it empty now.
-		
-	end
-	
 end
 
-
+function ENT:AcceptInput(Name, Activator, Caller)
+	if Name == "Use" and Caller:IsPlayer() then
+		umsg.Start("OpenHowto", Caller) -- Prepare the usermessage to that same player to open the menu on his side.
+		umsg.End() -- We don't need any content in the usermessage so we're sending it empty now.
+	end
+end
